cbcd721a4d4ecabbef91d53ed81deb0ff9b6a031
PedroGuilherme <pedro_races@hotmail.com>
2020-09-20 20:49:03 -0300

    fix bug in styles in responsive version

--- src/styles/reponsive.js
+++ src/styles/reponsive.js
@@ -3,9 +3,13 @@ import { createGlobalStyle } from "styled-components"
 export default createGlobalStyle`
   @media screen and (min-width:200px) and (max-width:1020px) {    
     .ContainerBox {
-      width:100%;
+      width:90%;
+      height:300px;
       background-color:#FEFEFE;
-      backdrop-filter:none;      
+      backdrop-filter:none;
+      left:20px;
+      right:0;
+      top:50px;
     }    
   }   
 `
